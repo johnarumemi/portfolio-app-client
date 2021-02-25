a2422f4ed455ed826c13da5b06168ac0d7b91578
John-AI <john.arumemi@gmail.com>
2021-02-25 18:53:18 +0000

    add: Header component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 
+import Header from './ui/Header';
 import theme from './ui/Theme';
 
 function App() {
   return (
       <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <Header />
         <Switch>
             {/* Main Routes */}
             <Route exact path='/' component={ () => ( <div>Home</div> ) } />
